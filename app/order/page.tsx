cfd71512ce311806b3b1ad1d8760ceb1a4e573f8
quanghien <nquanghien97@gmail.com>
2024-11-22 10:11:18 +0700

    add metadata

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -8,6 +8,33 @@ import React from 'react'
 import Image from 'next/image'
 import Banner from '@/components/HomePage/Banner'
 import Link from 'next/link'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://kidscareplus.vn'),
+  title: 'Kidscareplus',
+  description: 'KIDS CARE PLUS vừa bổ sung Canxi, Phospho, D3, K2 làm nguyên liệu cho quá trình tạo xương, vừa chứa thành phần CBP kích thích nguyên bào xương phát triển, nhờ ...',
+  keywords: 'Oz Farm Kid\'s Care Plus',
+  robots: {
+    follow: true,
+    index: true,
+  },
+  openGraph: {
+    locale: 'vi_VN',
+    title: 'Kids Care Plus',
+    url: 'https://kidscareplus.vn/',
+    siteName: 'KidsCarePlus',
+    type: 'website'
+  },
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        url: '/favicon.ico'
+      }
+    ]
+  }
+}
 
 function page() {
   return (
