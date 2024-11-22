dfa703b772c124ba7e61446ab1c82b0b6ead3205
quanghien <nquanghien97@gmail.com>
2024-11-22 10:26:30 +0700

    implement: order

--- entities/order.ts
+++ entities/order.ts
@@ -1,8 +1,8 @@
 export interface OrderEntity {
   fullName: string;
   phoneNumber: string;
-  productName: string;
-  quantity: number;
+  productName?: string;
+  quantity?: number;
   province?: string;
   district?: string;
   ward?: string;
